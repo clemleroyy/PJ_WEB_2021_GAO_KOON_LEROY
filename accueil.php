6f4af41e6634493bd5a91926c1c54fb7b448e52c
Jeycko <jimmy.koon@edu.ece.fr>
2021-12-07 14:05:17 +0100

    modif

--- accueil.php
+++ accueil.php
@@ -7,7 +7,47 @@
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
-   <?php include ('design.php') ?>
+   <header>
+         <h1 style="text-align: center;"><img src="logo_maison_manolo.png" width="200px" height="120px"></h1>
+      </header>
+      <nav class="navbar navbar-expand-sm sticky-top bg-light navbar-light">
+        <div class="container-fluid">
+            <a class="navbar-brand col-sm-2" style="width:200px" href="#">Maison Manolo</a>
+         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+            <span class="navbar-toggler-icon"></span>
+         </button>
+         <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav col-sm-2" style="width:200px">
+               <li class="nav-item">
+                  <a class="nav-link active" href="accueil.php">Accueil</a>
+               </li>
+            </ul>
+            <ul class="navbar-nav col-sm-2" style="width:250px">
+               <li class="nav-item">
+                  <a class="nav-link" href="parcourir.php">Tout parcourir</a>
+               </li>
+           </ul>
+           <ul class="navbar-nav col-sm-2" style="width:250px">
+               <li class="nav-item">
+                  <a class="nav-link" href="notification.php">Notification</a>
+               </li>
+            </ul>
+            <ul class="navbar-nav col-sm-2" style="width:200px">
+               <li class="nav-item">
+                  <a class="nav-link" href="panier.php">Panier</a>
+               </li>
+            </ul>
+            <ul class="navbar-nav col-sm-2" style="width:200px">
+               <li class="nav-item">
+                  <a class="nav-link" href="compte.php">Mon compte</a>
+               </li>
+            </ul>
+         </div>
+      </div>
+      </nav>
+      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
    <h1>TEST</h1>
    <h2>TEST2</h2>
    <?php include ('footer.php') ?>
