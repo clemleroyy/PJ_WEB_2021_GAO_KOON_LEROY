af62ecf472641942244e1020f9d4ba2cf784d305
Jeycko <jimmy.koon@edu.ece.fr>
2021-12-07 22:44:28 +0100

    modif + rajout image

--- panier.php
+++ panier.php
@@ -7,9 +7,6 @@
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
-   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
    <header>
          <h1 style="text-align: center;"><img src="logo_maison_manolo_v4.png" width="200px" height="120px"></h1>
       </header>
@@ -22,7 +19,7 @@
          <div class="collapse navbar-collapse" id="collapsibleNavbar">
             <ul class="navbar-nav col-sm-2" style="width:200px">
                <li class="nav-item">
-                  <a class="nav-link active" href="index.php">Accueil</a>
+                  <a class="nav-link" href="index.php">Accueil</a>
                </li>
             </ul>
             <ul class="navbar-nav col-sm-2" style="width:200px">
@@ -37,7 +34,7 @@
             </ul>
             <ul class="navbar-nav col-sm-2" style="width:200px">
                <li class="nav-item">
-                  <a class="nav-link" href="panier.php">Panier</a>
+                  <a class="nav-link active" href="panier.php">Panier</a>
                </li>
             </ul>
             <ul class="navbar-nav col-sm-2" style="width:200px">
@@ -48,6 +45,9 @@
          </div>
       </div>
       </nav>
+      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
    <br><br><br><br><br><br><br><br><br><br><br><br>
    <br><br><br><br><br><br><br><br><br><br><br><br>
    <br><br><br><br><br><br><br><br><br><br><br><br>
--- testindex.php
+++ testindex.php
@@ -7,9 +7,6 @@
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
-   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
    <header>
          <h1 style="text-align: center;"><img src="logo_maison_manolo_v4.png" width="200px" height="120px"></h1>
       </header>
@@ -48,6 +45,9 @@
          </div>
       </div>
       </nav>
+      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
    <br><br><br><br><br><br><br><br><br><br><br><br>
    <br><br><br><br><br><br><br><br><br><br><br><br>
    <br><br><br><br><br><br><br><br><br><br><br><br>
