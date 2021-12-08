42148b4842cf6ba9c137320a0e640597138fa8d9
Jeycko <jimmy.koon@edu.ece.fr>
2021-12-09 00:54:08 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,3 @@
-<?php
-   session_start()
-?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,12 +7,6 @@
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
-   <?php
-   // Set session variables (variables globales)
-   $_SESSION["test"] = "";
-   $_SESSION["statutPaiement"] = "void";
-
-   ?>
    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
@@ -23,33 +14,33 @@
          <h1 style="text-align: center;"><img src="logo_maison_manolo_v4.png" width="200px" height="120px"></h1>
    </header>
    <nav class="navbar navbar-expand-sm sticky-top bg-light navbar-light">
-      <div class="container-fluid">
-         <a class="navbar-brand col-sm-2" style="margin-left: 20px; width:180px" href="index.php"><img src="logo_maison_manolo_v4.png" width="80px" height="50px"></a>
+      <div class="container-fluid row">
+         <a class="navbar-brand col-sm-2" href="index.php"><img src="logo_maison_manolo_v4.png" width="80px" height="50px"></a>
          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
             <span class="navbar-toggler-icon"></span>
          </button>
          <div class="collapse navbar-collapse" id="collapsibleNavbar">
-            <ul class="navbar-nav col-sm-2" style="width:200px">
+            <ul class="navbar-nav col-sm-2">
                <li class="nav-item">
                   <a class="nav-link active" href="index.php">Accueil</a>
                </li>
             </ul>
-            <ul class="navbar-nav col-sm-2" style="width:200px">
+            <ul class="navbar-nav col-sm-2">
                <li class="nav-item">
                   <a class="nav-link" href="parcourir.php">Tout parcourir</a>
                </li>
            </ul>
-           <ul class="navbar-nav col-sm-2" style="width:200px">
+           <ul class="navbar-nav col-sm-2">
                <li class="nav-item">
                   <a class="nav-link" href="notification.php">Notification</a>
                </li>
             </ul>
-            <ul class="navbar-nav col-sm-2" style="width:200px">
+            <ul class="navbar-nav col-sm-2">
                <li class="nav-item">
                   <a class="nav-link" href="panier.php">Panier</a>
                </li>
             </ul>
-            <ul class="navbar-nav col-sm-2" style="width:200px">
+            <ul class="navbar-nav col-sm-2">
                <li class="nav-item">
                   <a class="nav-link" href="compte.php">Mon compte</a>
                </li>
