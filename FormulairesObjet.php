19314da80d4977641a3684d8a68c40a3edd57fd0
clemleroyy <57902783+clemleroyy@users.noreply.github.com>
2021-12-09 01:38:49 +0100

    formulaires objet+ header et footer

--- FormulairesObjet.php
+++ FormulairesObjet.php
@@ -2,82 +2,196 @@
 <html>
 <head>
    <meta charset="utf-8">
-   <title>Formulaire</title>
-<script type="text/javascript" src="https://gc.kis.v2.scr.kaspersky-labs.com/FD126C42-EBFA-4E12-B309-BB3FDD723AC1/main.js?attr=JtPqHA_wsxdjbfhgTaHABQ8pPh2_z3FAyoFcgQnTRqt-k1ImmA_iYGUBt0k7VQY8_NNmFonbEsDaViW-msOxBznHzGFBKBZ6ODAVvNENfb0vPxIqC9reoiFvmq3p1cHgBRTIUbE_qvjbySgut1nNwg" charset="UTF-8"></script></head>
-
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <title>Gérer les objets</title>
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
 <body>
-   <form action="1traitement.php" method="post">
-      <table border=2>
-         <tr>
-            <td colspan=2 align=center>
-               <strong>Ajouter un objet</strong></td>
-         </tr>
-         <tr>
-            <td>Catégorie :</td>
-            <td><input type ="radio" name="Categorie" value="Regulier"> Régulier<br>
-               <input type ="radio" name="Categorie" value="Rare"> Rare <br>
-            <input type ="radio" name="Categorie" value="Haut de gamme"> Haut de gamme<br></td>
-         </tr>
-         <tr>
-            <td>Nom de l'objet:</td>
-            <td><input type ="text" name="nomObjet"></td>
-         </tr>
-         <tr>
-            <td>Mode de vente :</td>
-            <td><input type ="radio" name="ModeV" value="Immediat"> Immédiat<br>
-               <input type ="radio" name="ModeV" value="Par meilleure offre"> Par meilleure offre <br>
-            <input type ="radio" name="ModeV" value="Par negociation"> Par négociation<br></td>
-         </tr>
-         <tr>
-            <td>Fin d'enchère:</td>
-            <td><input type ="date" name="FinEnchere"></td>
-         </tr>
-         <tr>
-            <td>Lien photo 1:</td>
-            <td><input type ="url" name="Photo1"></td>
-         </tr>
-         <tr>
-            <td>Lien photo 2:</td>
-            <td><input type ="url" name="Photo2"></td>
-         </tr>
-         <tr>
-            <td>Lien photo 3:</td>
-            <td><input type ="url" name="Photo3"></td>
-         </tr>
-         <tr>
-            <td>Lien vidéo:</td>
-            <td><input type ="url" name="Video"></td>
-         </tr>
-         
+   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+   <header>
+         <h1 style="text-align: center;"><img src="logo_maison_manolo_v4.png" width="200px" height="120px"></h1>
+   </header>
+   <nav class="navbar navbar-expand-sm sticky-top bg-light navbar-light">
+      <div class="container-fluid">
+         <a class="navbar-brand col-sm-2" style="margin-left: 25px; width:180px" href="index.php"><img src="logo_maison_manolo_v4.png" width="80px" height="50px"></a>
+         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+            <span class="navbar-toggler-icon"></span>
+         </button>
+         <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav col-sm-2" style="width:200px">
+               <li class="nav-item">
+                  <a class="nav-link active" href="index.php">Accueil</a>
+               </li>
+            </ul>
+            <ul class="navbar-nav col-sm-2" style="width:200px; ">
+               <li class="nav-item">
+                  <a class="nav-link" href="parcourir.php">Tout parcourir</a>
+               </li>
+           </ul>
+           <ul class="navbar-nav col-sm-2" style="width:200px">
+               <li class="nav-item">
+                  <a class="nav-link" href="notification.php">Notification</a>
+               </li>
+            </ul>
+            <ul class="navbar-nav col-sm-2" style="width:200px">
+               <li class="nav-item">
+                  <a class="nav-link" href="panier.php">Panier</a>
+               </li>
+            </ul>
+            <ul class="navbar-nav col-sm-2" style="width:200px">
+               <li class="nav-item">
+                  <a class="nav-link" href="compte.php">Mon compte</a>
+               </li>
+            </ul>
+         </div>
+      </div>
+   </nav>
+   <main>
+
+<br>
+<h1><strong>Ajout d'un objet</strong></h1>
+<br>
+<form>
    
-   <tr>
-      <td colspan="2" align="center">
-         <input type="submit" value="Soumettre"></td>
-   </tr>
-                   
-         
+   <fieldset class="form-group">
+    <div class="row" style="padding-left: 10px">
+      <legend class="col-form-label col-sm-2 pt-0">Catégorie</legend>
+      <div class="col-sm-10">
+        <div class="form-check">
+          <input class="form-check-input" type="radio" name="Categorie" id="gridRadios1" value="Regulier">
+          <label class="form-check-label" for="gridRadios1">
+            Régulier
+          </label>
+        </div>
+        <div class="form-check">
+          <input class="form-check-input" type="radio" name="Categorie" id="gridRadios2" value="Rare">
+          <label class="form-check-label" for="gridRadios2">
+            Rare
+          </label>
+        </div>
+        <div class="form-check disabled">
+          <input class="form-check-input" type="radio" name="Categorie" id="gridRadios3" value="HautDeGamme">
+          <label class="form-check-label" for="gridRadios3">
+            Haut de gamme
+          </label>
+        </div>
+      </div>
+    </div>
+  </fieldset>
+  <br>
+  
+  <div class="form-group row" style="padding-left: 10px">
+    <label for="inputEmail3" class="col-sm-2 col-form-label">Nom de l'objet</label>
+    <div class="col-2">
+      <input type="text" class="form-control" id="inputEmail3" placeholder="Nom de l'objet" required>
+    </div>
+  </div>
+  <br>
 
-      </table>
+  
+  <fieldset class="form-group">
+    <div class="row" style="padding-left: 10px">
+      <legend class="col-form-label col-sm-2 pt-0">Mode de vente</legend>
+      <div class="col-sm-10">
+        <div class="form-check">
+          <input class="form-check-input" type="radio" name="ModeDeVente" id="gridRadios1" value="Immediat">
+          <label class="form-check-label" for="gridRadios1">
+            Immédiat
+          </label>
+        </div>
+        <div class="form-check">
+          <input class="form-check-input" type="radio" name="ModeDeVente" id="gridRadios2" value="MeilleureOffre">
+          <label class="form-check-label" for="gridRadios2">
+            Par meilleure offre
+          </label>
+        </div>
+        <div class="form-check disabled">
+          <input class="form-check-input" type="radio" name="ModeDeVente" id="gridRadios3" value="Negociation">
+          <label class="form-check-label" for="gridRadios3">
+            Par négociation
+          </label>
+        </div>
+      </div>
+    </div>
+  </fieldset>
+  <br>
+  
+  <div class="form-group row" style="padding-left: 10px">
+    <label for="inputPassword3" class="col-sm-2 col-form-label">Date fin d'enchère</label>
+    <div class="col-2">
+      <input type="date" class="form-control" id="inputPassword3" placeholder="Lien" required>
+    </div>
+  </div>
+  <br>
 
-      <table border=2>
-         <tr>
-            <td colspan=2 align=center>
-               <strong>Supprimer un objet</strong></td>
-         </tr>
-         <tr>
-         <tr>
-            <td>ID de l'objet:</td>
-            <td><input type ="num" name="IDObjet"></td>
-         </tr>
-   <tr>
-      <td colspan="2" align="center">
-         <input type="submit" value="Supprimer"></td>
-   </tr>
-                   
-         
+  <div class="form-group row" style="padding-left: 10px">
+    <label for="inputPassword3" class="col-sm-2 col-form-label">Lien (1ère photo)</label>
+    <div class="col-2">
+      <input type="url" class="form-control" id="inputPassword3" placeholder="Lien" required>
+    </div>
+  </div>
+  <br>
 
-      </table>
-   </form>
+  <div class="form-group row" style="padding-left: 10px">
+    <label for="inputPassword3" class="col-sm-2 col-form-label">Lien (2ème photo)</label>
+    <div class="col-2">
+      <input type="url" class="form-control" id="inputPassword3" placeholder="Lien" required>
+    </div>
+  </div>
+  <br>
 
+  <div class="form-group row" style="padding-left: 10px">
+    <label for="inputPassword3" class="col-sm-2 col-form-label">Lien (3ème photo)</label>
+    <div class="col-2">
+      <input type="url" class="form-control" id="inputPassword3" placeholder="Lien" required>
+    </div>
+  </div>
+  <br>
+
+  <div class="form-group row" style="padding-left: 10px">
+    <label for="inputPassword3" class="col-sm-2 col-form-label">Lien (vidéo)</label>
+    <div class="col-2">
+      <input type="url" class="form-control" id="inputPassword3" placeholder="Lien" required>
+    </div>
+  </div>
+  <br>
+
+  <div class="form-group row" style="padding-left: 10px">
+    <div class="col-sm-10" style="padding-left: 15%">
+      <button type="submit" class="btn btn-primary">Ajouter</button>
+    </div>
+  </div>
+</form>
+<br>
+<br>
+
+<h1><strong>Suppression d'un objet</strong></h1>
+<br>
+<form>
+   
+  <br>
+  
+  <div class="form-group row" style="padding-left: 10px">
+    <label for="inputEmail3" class="col-sm-2 col-form-label">ID de l'objet</label>
+    <div class="col-2">
+      <input type="number" class="form-control" id="inputEmail3" placeholder="ID de l'objet" required>
+    </div>
+  </div>
+  <br>
+  <div class="form-group row" style="padding-left: 10px">
+    <div class="col-sm-10" style="padding-left: 15%">
+      <button type="submit" class="btn btn-primary">Supprimer</button>
+    </div>
+  </div>
+  
+</form>
+<br>
+<br>
+
+   </main>
+   <footer>
+      <?php include ('footer.php') ?>
+   </footer>
 </body>
-</html>
