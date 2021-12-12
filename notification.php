6c871f0705b58978f8bc9e78d02a5aeed3803df4
Jeycko <jimmy.koon@edu.ece.fr>
2021-12-12 03:04:41 +0100

    modif

--- notification.php
+++ notification.php
@@ -10,38 +10,42 @@
 
 
 <body>
+   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
    <header>
          <h1 style="text-align: center;"><img src="logo_maison_manolo_v4.png" width="200px" height="120px"></h1>
       </header>
-      <nav class="navbar navbar-expand-sm sticky-top bg-light navbar-light">
-        <div class="container-fluid">
-            <a class="navbar-brand col-sm-2" style="margin-left: 20px; width:180px" href="index.php"><img src="logo_maison_manolo_v4.png" width="80px" height="50px"></a>
+      <nav class="navbar navbar-expand-lg sticky-top bg-light navbar-light">
+      <div class="container-fluid">
+         <a class="navbar-brand col-sm-2" style="margin-left: 25px;" href="index.php"><img src="logo_maison_manolo_v4.png" width="80px" height="50px"></a>
          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
             <span class="navbar-toggler-icon"></span>
          </button>
          <div class="collapse navbar-collapse" id="collapsibleNavbar">
-            <ul class="navbar-nav col-sm-2" style="width:200px">
-               <li class="nav-item">
+            <ul class="navbar-nav  col-sm-2">
+               <li class="nav-item px-5">
                   <a class="nav-link" href="index.php">Accueil</a>
                </li>
             </ul>
-            <ul class="navbar-nav col-sm-2" style="width:200px">
-               <li class="nav-item">
+            <ul class="navbar-nav  col-sm-2">
+               <li class="nav-item px-5">
                   <a class="nav-link" href="parcourir.php">Tout parcourir</a>
                </li>
            </ul>
-           <ul class="navbar-nav col-sm-2" style="width:200px">
-               <li class="nav-item">
+           <ul class="navbar-nav  col-sm-2">
+               <li class="nav-item px-5">
                   <a class="nav-link active" href="notification.php">Notification</a>
                </li>
             </ul>
-            <ul class="navbar-nav col-sm-2" style="width:200px">
-               <li class="nav-item">
-                  <a class="nav-link" href="panier.php">Panier</a>
+            <ul class="navbar-nav  col-sm-2">
+               <li class="nav-item px-5">
+                  <a class="nav-link" href="panier.php">Panier<img src="panier.png" style="width:15px; height:15px"></a>
+
                </li>
             </ul>
-            <ul class="navbar-nav col-sm-2" style="width:200px">
-               <li class="nav-item">
+            <ul class="navbar-nav  col-sm-2">
+               <li class="nav-item px-5">
                   <a class="nav-link" href="compte.php">Mon compte</a>
                </li>
             </ul>
